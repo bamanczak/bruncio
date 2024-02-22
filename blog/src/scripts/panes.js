f433a6807ebdfe7299dafd5936dbc528da46582a
Bruno Mańczak <bamanczak@gmail.com>
2024-02-22 09:30:12 +0100

    no drag on close button

--- blog/src/scripts/panes.js
+++ blog/src/scripts/panes.js
@@ -1,32 +1,43 @@
-const panes = document.querySelectorAll('.pane')
-
-let z = 1
-const margin = 10
+const panes = document.querySelectorAll('.pane');
+let z = 1;
+const margin = 10;
 
 panes.forEach((pane) => {
-  const title = pane.querySelector('.title')
-  const corner = pane.querySelector('.corner')
+  const title = pane.querySelector('.title');
+  const corner = pane.querySelector('.corner');
 
   pane.addEventListener('mousedown', () => {
-    z = z + 1
-    pane.style.zIndex = z
+    z = z + 1;
+    pane.style.zIndex = z;
   })
 
   title.addEventListener('mousedown', (event) => {
-    pane.querySelector('.change-on-drag').classList.add('is-dragging')
-    // pane.find('.title').classList.add('is-dragging')
-    pane.classList.add('is-dragging')
+    const myButton = title.querySelector('.baton');
 
-    let l = pane.offsetLeft
-    let t = pane.offsetTop
+    let buttonLeft = myButton.offsetLeft;
+    // Why figure out why calculations are wrong if I can just hardcode the missing pixels?
+    let buttonTop = myButton.offsetTop + 2;
+    let buttonWidth = myButton.offsetWidth + 3;
+    let buttonHeight = myButton.offsetHeight;
 
-    let startX = event.pageX
-    let startY = event.pageY
+    let l = pane.offsetLeft;
+    let t = pane.offsetTop;
+
+    let startX = event.pageX;
+    let startY = event.pageY;
     let screenWidth = document.documentElement.clientWidth;
 
+    let clashX = false;
+    let clashY = false;
+    if (startX > (buttonLeft + l) && startX < (buttonLeft + l + buttonWidth)) {
+      clashX = true;
+    }
+    if (startY > (buttonTop + t) && startY < (buttonTop + t + buttonHeight)) {
+      clashY = true;
+    }
 
     const drag = (event) => {
-      event.preventDefault()
+      event.preventDefault();
 
       let newValueLeft = l + (event.pageX - startX);
       if (newValueLeft < 0) {
@@ -47,37 +58,46 @@ panes.forEach((pane) => {
     }
 
     const mouseup = () => {
-      pane.querySelector('.change-on-drag').classList.remove('is-dragging')
-      pane.classList.remove('is-dragging')
+      pane.querySelector('.change-on-drag').classList.remove('is-dragging');
+      pane.classList.remove('is-dragging');
 
-      document.removeEventListener('mousemove', drag)
-      document.removeEventListener('mouseup', mouseup)
+      document.removeEventListener('mousemove', drag);
+      document.removeEventListener('mouseup', mouseup);
     }
 
-    document.addEventListener('mousemove', drag)
-    document.addEventListener('mouseup', mouseup)
+    let canDrag = true;
+    if (clashX && clashY) {
+      canDrag = false;
+    }
+    if (canDrag) {
+      pane.classList.add('is-dragging');
+      document.addEventListener('mousemove', drag);
+      document.addEventListener('mouseup', mouseup);
+
+    }
+
   })
 
   corner.addEventListener('mousedown', (event) => {
-    let w = pane.clientWidth
-    let h = pane.clientHeight
+    let w = pane.clientWidth;
+    let h = pane.clientHeight;
 
-    let startX = event.pageX
-    let startY = event.pageY
+    let startX = event.pageX;
+    let startY = event.pageY;
 
     const drag = (event) => {
-      event.preventDefault()
+      event.preventDefault();
 
-      pane.style.width = w + (event.pageX - startX) + 'px'
-      pane.style.height = h + (event.pageY - startY) + 'px'
+      pane.style.width = w + (event.pageX - startX) + 'px';
+      pane.style.height = h + (event.pageY - startY) + 'px';
     }
 
     const mouseup = () => {
-      document.removeEventListener('mousemove', drag)
-      document.removeEventListener('mouseup', mouseup)
+      document.removeEventListener('mousemove', drag);
+      document.removeEventListener('mouseup', mouseup);
     }
 
-    document.addEventListener('mousemove', drag)
-    document.addEventListener('mouseup', mouseup)
+    document.addEventListener('mousemove', drag);
+    document.addEventListener('mouseup', mouseup);
   })
 })
