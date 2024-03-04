5552b220b4001d7b6731bb357632f134d7860ada
Bruno Mańczak <bamanczak@gmail.com>
2024-03-04 18:04:40 +0100

    stuff

--- blog/src/components/MenuDropdown.svelte
+++ blog/src/components/MenuDropdown.svelte
@@ -31,8 +31,9 @@
                 on:click={() => {
                     activatePanel("A11y");
                     activatePanel("CokkieNotice", 300);
-                    activatePanel("PrivacyPolicy", 600);
+                    activatePanel("EnvPolicy", 600);
                     activatePanel("TermsOfUse", 900);
+                    activatePanel("PrivacyPolicy", 1200);
                 }}
                 class="legal-opener flex items-center gap-x-0.5 py-2 px-3 text-sm text-black hover:text-mywhite hover:bg-black focus:outline-none focus:bg-black focus:text-mywhite"
                 >Legal</MenuItem
---
+++ blog/src/pages/special/environment.md
@@ -0,0 +1,6 @@
+# Environmental impact
+
+This website has no measured environmental impact.
+However, the human behind it might have negative environmental impact. Aforementioned human has solar panels on his roof and air source heat pump. On the other hand, he drives a car on a regular basis and buys new electronics every few years. 
+
+It is worth noticing though, that this website, similarly to [Peanut Butter](https://improbable.com/airchives/classical/articles/peanut_butter_rotation.html) has has no effect on the rotation of the earth.
