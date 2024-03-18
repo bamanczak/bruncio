d4914a8fa92d0f323fa038443f79f67b5012e289
Bruno Mańczak <bamanczak@gmail.com>
2024-03-18 10:23:00 +0100

    vercel

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -3,9 +3,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
 import icon from "astro-icon";
 
-import svelte from "@astrojs/svelte";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,8 +15,13 @@ export default defineConfig({
     include: {
       mdi: ["*"],
       // (Default) Loads entire Material Design Icon set
-      "material-symbols": ["*"], // (Default) Loads entire Material Design Icon set
+      "material-symbols": ["*"],
+      // (Default) Loads entire Material Design Icon set
       "pixelarticons": ["*"]
     }
-  }), svelte()]
+  }), svelte()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  })
 });
