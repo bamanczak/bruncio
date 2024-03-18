eb84006bd63a6988f2fec874402a5a2dde88b4c4
Bruno Mańczak <bamanczak@gmail.com>
2024-03-18 11:12:38 +0100

    fontfix

--- blog/tailwind.config.mjs
+++ blog/tailwind.config.mjs
@@ -49,7 +49,6 @@ export default {
 			fontFamily: {
 				sans: [
 					'PixChicago', ...defaultTheme.fontFamily.sans
-					// 'Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji',
 				],
 			},
 		},
